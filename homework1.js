b529cceb66a4b22d5c440a576962c97dbc01cacf
kennedygzz04-art <kennedygzz04@gmail.com>
2026-02-27 17:33:02 -0600

    commit

--- homework1.js
+++ homework1.js
@@ -1,9 +1,9 @@
 /*
-    Name: Kennedy Gonzalez
-    Date created:
-    Date Modified
-    Version:
-    Summary: 
+    Name: Kennedy Gonzalez'
+    Date created:2/24/2026
+    Date last updated: 2/27/2026
+    Version: 1.3
+    Description: This document is a patient registration form for grammy winner Megan Thee Stallion's new clinic she has funded.
 */
 
 //dynamic date js code
